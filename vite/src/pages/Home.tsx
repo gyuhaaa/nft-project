e2d4693414886875ffcb855f264dfbb77df3de26
gyuhaaa <vkvkvk2892@gmail.com>
2024-06-17 14:28:47 +0900

    Log in/out

--- vite/src/pages/Home.tsx
+++ vite/src/pages/Home.tsx
@@ -2,7 +2,7 @@ import { Flex } from "@chakra-ui/react";
 import { FC } from "react";
 
 const Home: FC = () => {
-  return <Flex>Home</Flex>;
+  return <Flex>Homepage description</Flex>;
 };
 
 export default Home;
--- vite/src/pages/My.tsx
+++ vite/src/pages/My.tsx
@@ -2,7 +2,12 @@ import { Flex } from "@chakra-ui/react";
 import { FC } from "react";
 
 const My: FC = () => {
-  return <Flex>My</Flex>;
+  return (
+    <Flex flexDir={"column"}>
+      <Flex>My Rank</Flex>
+      <Flex>진행도</Flex>
+    </Flex>
+  );
 };
 
 export default My;
